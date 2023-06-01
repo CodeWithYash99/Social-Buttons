d96a6104d184dc0bd8a73139c975d345653d6e72
CodeWithYAsh99 <yashwanthrangu99@gmail.com>
2023-06-01 17:58:44 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import SocialButtons from './components/SocialButtons'
+import './App.css';
+
+const App =() => (
+    <div className="app-container">
+      <SocialButtons />
+    </div>
+  )
+
+export default App;
---
+++ src/components/SocialButtons/index.js
@@ -0,0 +1,16 @@
+import "./index.css"
+
+const SocialButtons = () => (
+    <div className="social-buttons-container">
+        <div className="content-container">
+            <h1 className='heading'>Social Buttons</h1>
+            <div className="buttons-container">
+                <button className="like-button button">Like</button>
+                <button className="comment-button button">Comment</button>
+                <button className="share-button button">Share</button>
+            </div>
+        </div>
+    </div>
+)
+
+export default SocialButtons
